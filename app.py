c977c587c5972ea329d6d9d2a2a9d99e1a95da28
michael9999 <michaelsmuts@gmail.com>
2023-03-01 17:30:40 +0100

    updated app.py

--- app.py
+++ app.py
@@ -20,3 +20,34 @@ def add_inputs():
     add.delay(x, y)
     flash("Your addition job has been submitted.")
     return redirect('/')
+
+
+# my changes from here
+
+@app.route('/fullSearch', methods=['POST'])
+def goFullSearch():
+
+    headers = request.headers
+    auth = headers.get("X-Api-Key")
+
+    if auth == 'asoidewfoef':
+        print("goFullSearch running")
+
+        #searchpapi = request.get_json().get('searchname', '')
+
+        #searchname = (searchpapi,)
+        
+        #task = run_FullSearch.apply_async(searchname, countdown=10)
+
+        task = run_FullSearch.apply_async(countdown=10)
+      
+        test_id = task.id
+
+        return test_id
+
+    else:
+        return jsonify({"message": "ERROR: Unauthorized"}), 401
+    
+@app.task
+def run_FullSearch(searchid):
+    print("run_FullSearch task is running")
